f6d2d33e2a2089bd3bff2b607a50177980e460c2
Sarthak <sarthakvyassv07@gmail.com>
2026-03-04 14:10:52 +0530

    Add unified snake-io game with enhanced features: multiplayer, start screen, leaderboard, minimap, 15+ bots

---
+++ public/games/snake-io/src/botSnake.js
@@ -0,0 +1,33 @@
+/**
+ * Bot extension of the core snake
+ * @param  {Phaser.Game} game      game object
+ * @param  {String} spriteKey Phaser sprite key
+ * @param  {Number} x         coordinate
+ * @param  {Number} y         coordinate
+ * @param  {String} name      bot name
+ */
+BotSnake = function(game, spriteKey, x, y, name) {
+    Snake.call(this, game, spriteKey, x, y, name || 'Bot');
+    this.trend = 1;
+    this.isBot = true;
+}
+
+BotSnake.prototype = Object.create(Snake.prototype);
+BotSnake.prototype.constructor = BotSnake;
+
+/**
+ * Add functionality to the original snake update method so that this bot snake
+ * can turn randomly
+ */
+BotSnake.prototype.tempUpdate = BotSnake.prototype.update;
+BotSnake.prototype.update = function() {
+    this.head.body.setZeroRotation();
+
+    //ensure that the bot keeps rotating in one direction for a
+    //substantial amount of time before switching directions
+    if (Util.randomInt(1,20) == 1) {
+        this.trend *= -1;
+    }
+    this.head.body.rotateRight(this.trend * this.rotationSpeed);
+    this.tempUpdate();
+}
